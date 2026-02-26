4dc2d5cd280d76d9c18da9a702a5de897efd249c
Robroi Neo M. Dingal <robroineo2@gmail.com>
2026-02-26 10:47:32 +0800

    Feat: add util db_loader.py to load sqlite db

---
+++ src/utils/db_loader.py
@@ -0,0 +1,36 @@
+import sqlite3
+from typing import List, Dict, Any
+
+DB_PATH = "cleaned_articles.db"
+TABLE_NAME = "articles"
+
+def load_articles_from_db(db_path: str = DB_PATH) -> List[Dict[str, Any]]:
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute(f"""
+        SELECT pageid, title, category, source_url, cleaned_text
+        FROM {TABLE_NAME}
+    """)
+
+    rows = cursor.fetchall()
+    conn.close()
+
+    articles = []
+    for row in rows:
+        articles.append({
+            "pageid": row["pageid"],
+            "title": row["title"],
+            "category": row["category"],
+            "source_url": row["source_url"],
+            "cleaned_text": row["cleaned_text"],
+        })
+
+    return articles
+
+if __name__ == "__main__":
+    db = load_articles_from_db()
+
+    if db:
+        print("Database Loaded Succesfully!")
